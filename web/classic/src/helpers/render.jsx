9aaf617fd498eae449308381bcae8b5922cdbc2c
Donzin <3439651960@qq.com>
2026-05-03 05:40:25 +0800

    fix(classic): restore FaLinkedinIn import for linkedin icon

--- web/classic/src/helpers/render.jsx
+++ web/classic/src/helpers/render.jsx
@@ -105,6 +105,7 @@ import {
   SiWechat,
   SiX,
 } from 'react-icons/si';
+import { FaLinkedinIn } from 'react-icons/fa6';
 
 // 获取侧边栏Lucide图标组件
 export function getLucideIcon(key, selected = false) {
@@ -508,7 +509,7 @@ const oauthProviderIconMap = {
   google: SiGoogle,
   discord: SiDiscord,
   facebook: SiFacebook,
-  linkedin: SiLinkedin,
+  linkedin: FaLinkedinIn,
   x: SiX,
   twitter: SiX,
   slack: SiSlack,
